f971402242a3ef7f1a90720d1655f47b6ede0454
efiw <waissman@transmitsecurity.com>
2019-09-22 22:31:10 +0300

    test2

--- rxdownload4/src/main/java/zlc/season/rxdownload4/utils/LogUtil.kt
+++ rxdownload4/src/main/java/zlc/season/rxdownload4/utils/LogUtil.kt
@@ -8,6 +8,8 @@ const val LOG_TAG = "RxDownload"
 
 fun <T> T.log(prefix: String = ""): T {
     if (LOG_ENABLE) {
+
+        // work work work...
         if (this is Throwable || Error ) {
             Log.w(LOG_TAG, prefix + this.message, this)
         } else {
